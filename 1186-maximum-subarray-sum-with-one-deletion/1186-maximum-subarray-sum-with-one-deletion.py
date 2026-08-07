dd9f895d0d4288475b5f54c508460cc499b4be2e
Aditya Ghatge <code.learning.2835@gmail.com>
2026-08-07 22:00:10 +0530

    Time: 19 ms (68.87%), Space: 27.9 MB (31.73%) - LeetHub

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.py
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.py
@@ -1,21 +1,17 @@
 class Solution:
     def maximumSum(self, arr: List[int]) -> int:
-        nodelete=arr[0]
-        onedelete=float('-inf')
-        ans=arr[0]
+        noDel = arr[0]
+        oneDel = float('-inf')
+        res = arr[0]
 
-        for i in range(1,len(arr)):
-            prevnodelete=nodelete
-            prevonedelete=onedelete
-            
-            nodelete=max(nodelete+arr[i],arr[i])
-            
-            if prevonedelete==float('-inf'):
-                v1=arr[i]
-            else:v1= prevonedelete+arr[i]
+        for i in range(1, len(arr)):
+            prevNoDel = noDel
 
-            onedelete=max(v1,prevnodelete)
+            noDel = max(arr[i], noDel + arr[i])
 
-            ans=max(ans,max(onedelete,nodelete))
+            oneDel = max(prevNoDel, oneDel + arr[i])
 
-        return ans
+            res = max(res, noDel, oneDel)
+
+        return res
+
