3f3142865950e153e8b67589c2c08ec6485bf512
Aditya Ghatge <code.learning.2835@gmail.com>
2026-09-24 10:11:04 +0530

    Time: 0 ms (100%), Space: 31.5 MB (8.28%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -3,7 +3,7 @@ class Solution:
         left,right=0,len(arr)-1
 
         while left<right:
-            mid=(left+right)//2
+            mid=left+(right-left)//2
 
             if arr[mid]<arr[mid+1]:
                 left=mid+1
