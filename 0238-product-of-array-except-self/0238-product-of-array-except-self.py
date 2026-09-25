960085e0e029c5c35e0d2fc736bf3f4258233eb1
Aditya Ghatge <code.learning.2835@gmail.com>
2026-09-25 19:47:31 +0530

    Time: 15 ms (92.38%), Space: 25.5 MB (54.09%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -7,7 +7,7 @@ class Solution:
         for i in range(n):
             res[i]=left
             left*=nums[i]
-        
+
         right=1
         for i in range(n-1,-1,-1):
             res[i]*=right
